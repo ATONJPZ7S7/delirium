784bf48be89461dc591b03d07a35befc6ad64e36
DeviX <54741732+ATONJPZ7S7@users.noreply.github.com>
2019-11-25 21:50:28 -0300

    Update version.h

--- src/version.h
+++ src/version.h
@@ -11,16 +11,16 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 72951;
+static const int PROTOCOL_VERSION = 72952;
 
 //! initial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 72951;
+static const int INIT_PROTO_VERSION = 72952;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 72951;
+static const int GETHEADERS_VERSION = 72952;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 72951;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 72952;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 72952;
 
 //! nTime field added to CAddress, starting with this version;
